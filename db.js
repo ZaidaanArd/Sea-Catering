2bc7ba97ad68b72aff87b0ceb41254f836d41f7d
ZaidaanArd <zaidaanardiansyah@gmail.com>
2025-06-28 18:55:59 +0700

    Configure MySQL connection pool in db.js

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
+// ============================
+//      MySQL Database Pool
+// ============================
+
 const mysql = require('mysql2');
 
+// Konfigurasi pool koneksi ke database MySQL
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',        // default XAMPP/Laragon
-  password: '',        // default XAMPP/Laragon biasanya kosong
+  user: 'root',           
+  password: '',           
   database: 'sea-catering',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
+// Export pool dengan promise-support
 module.exports = pool.promise();
